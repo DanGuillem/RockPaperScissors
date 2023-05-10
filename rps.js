603c99ed4fa52e7f03aae093e7672a8f38758e20
DanGuillem <daniel.guillem@hotmail.co.uk>
2023-05-10 13:40:21 +0100

    Added UI for RPS Game

--- rps.js
+++ rps.js
@@ -4,81 +4,120 @@ function getComputerChoice() {
     return rps[randRps];    
 };
 
+    const buttons = document.querySelectorAll('button');
 
-function playRound() {
+    buttons.forEach((button) => {
+
+        button.addEventListener('click', () => {
+            game(button.id);
+        });
+
+    });
+
+const results = document.querySelector('#results');
+
+let userScore = 0;
+let compScore = 0;
+
+
+function playRound(playerSelection) {
     let compChoice = getComputerChoice();
 
-    let playerSelection = prompt("Please enter your guess: ");
-
-    if (playerSelection.toLowerCase() === 'rock') {
+    if (playerSelection === 'rock') {
         if (compChoice === 'Paper') {
-            console.log('Computer chose Paper, you lose!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Paper, you lose!'
+            results.appendChild(resultUpdate)
             return result = 'compWin';
         }
         else if (compChoice === 'Scissors') {
-            console.log('Computer chose Scissors, you win!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Scissors, you win!'
+            results.appendChild(resultUpdate)
             return 'userWin';
         }
         else if (compChoice === 'Rock') {
-            console.log('Computer chose Rock, you draw')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Rock, you draw'
+            results.appendChild(resultUpdate)
             return 'draw';
         }
     }
     
-    else if (playerSelection.toLowerCase() === 'paper') {
+    else if (playerSelection === 'paper') {
         if (compChoice === 'Rock') {
-            console.log('Computer chose Rock, you win!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Rock, you win!'
+            results.appendChild(resultUpdate)
             return 'userWin';
         }
         else if (compChoice === 'Scissors') {
-            console.log('Computer chose Scissors, you lose!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Scissors, you lose!'
+            results.appendChild(resultUpdate)
             return 'compWin';
         }
         else if (compChoice === 'Paper') {
-            console.log('Computer chose Paper, you draw')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Paper, you draw'
+            results.appendChild(resultUpdate)
             return 'draw';
         }
     }
     
-    else if (playerSelection.toLowerCase() === 'scissors') {
+    else if (playerSelection === 'scissors') {
         if (compChoice === 'Rock') {
-            console.log('Computer chose Rock, you lose!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Rock, you lose!'
+            results.appendChild(resultUpdate)
             return 'compWin';
         }
         else if (compChoice === 'Scissors') {
-            console.log('Computer chose Scissors, you draw!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Scissors, you draw!'
+            results.appendChild(resultUpdate)
             return 'draw';
         }
         else if (compChoice === 'Paper') {
-            console.log('Computer chose Paper, you win!')
+            let resultUpdate = document.createElement('div')
+            resultUpdate.textContent = 'Computer chose Paper, you win!'
+            results.appendChild(resultUpdate)
             return 'userWin';
         }
     }
 }
 
-function game() {
-    let userScore = 0;
-    let compScore = 0;
+function game(playerSelection) {
 
-    for (let i = 0; i < 5; i++) {
-        let round = playRound();
-        if (round === "userWin") {
-            userScore++;
-            console.log(`Current Score: You: ${userScore}, Computer: ${compScore}`);
-        } else if (round === "compWin") {
-            compScore++;
-            console.log(`Current Score: You: ${userScore}, Computer: ${compScore}`);
-        } else if (round === "draw") {
-            console.log(`Current Score: You: ${userScore}, Computer: ${compScore}`);
-        }
+    const playerScoreDisplay = document.querySelector('#playerscore');
+    const compScoreDisplay = document.querySelector('#compscore');
+
+    function updateScore(){
+        playerScoreDisplay.textContent = `Player Score: ${userScore}`;
+        compScoreDisplay.textContent = `Computer Score: ${compScore}`;
     }
 
-    if (userScore > compScore) {
-        console.log('Game Over! Congrats! You Win!');
-    } else if (userScore < compScore) {
-        console.log('Game Over! You Lose!'); 
-    } else {
-        console.log("Game Over! Boring, It's a draw!");
+    let round = playRound(playerSelection);
+        if (round === "userWin") {
+            userScore++;
+            updateScore();
+        } else if (round === "compWin") {
+            compScore++;
+            updateScore();
+        } else if (round === "draw") {
+            updateScore();
+        }
+    
+
+    if (userScore === 5) {
+        const playerWin = document.createElement('h1');
+        playerWin.textContent = 'Congrats! You Win!';
+        results.appendChild(playerWin);
+    }
+    else if (compScore === 5) {
+        const playerWin = document.createElement('h1');
+        playerWin.textContent = 'Computer wins';
+        results.appendChild(playerWin);
     }
 
 }
